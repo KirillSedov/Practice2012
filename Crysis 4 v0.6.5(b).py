7cade5c327edcba82fe34d74636ebb6f252adaef
KirillSedov <Kirill.Sedov@2011.auditory.ru>
2012-07-19 22:45:33 +0400

    Update Crysis 4
    Переделана анимация перса, добавлена лава,доделан переход на следующий
    лвл, подправленны предыдущие функции.

--- Crysis 4 v0.6.5(b).py
+++ Crysis 4 v0.6.5(b).py
@@ -8,7 +8,6 @@ def load_image(name):
    return i.convert_alpha()
 
 def platform_func(x_speed):#Стены
-   global level
    if x_speed > 0:
         if level.get_at(((x_cord+32)/32, (y_cord+16)/32)) <> (empty):
           return 0
@@ -20,11 +19,6 @@ def platform_func(x_speed):#Стены
    if x_speed == 0:return 0
 
 def gravity_func(gravity):#Гравитация
-   global level
-   if gravity == 0:
-      if level.get_at(((x_cord+16)/32, (y_cord+32)/32)) <> i:
-           return 0
-      return 16
    if gravity > 0:
        if level.get_at(((x_cord+16)/32, (y_cord+32)/32)) <> (empty) or level.get_at(((x_cord)/32, (y_cord+32)/32)) <> (empty):
           return 0
@@ -34,6 +28,10 @@ def gravity_func(gravity):#Гравитация
           return 0
        if level.get_at(((x_cord)/32, (y_cord+32)/32)) <> (empty) and level.get_at(((x_cord)/32, (y_cord-32)/32)) <> (empty):
           return 0
+       if level.get_at(((x_cord+16)/32, (y_cord+32)/32)) <> (empty) and level.get_at(((x_cord)/32, (y_cord-32)/32)) <> (empty):
+          return 0
+       if level.get_at(((x_cord)/32, (y_cord+32)/32)) <> (empty) and level.get_at(((x_cord+16)/32, (y_cord-32)/32)) <> (empty):
+          return 0
        if level.get_at(((x_cord+16)/32, (y_cord-32)/32)) <> (empty) or level.get_at(((x_cord)/32, (y_cord-32)/32)) <> (empty):
           return 16
        return -32
@@ -42,15 +40,14 @@ def jump( gravity):#Прыжок
    global jump_height, jump_trigger
    if gravity > 0:
       jump_trigger = False
-   if  gravity == 0:#Алгоритм неочень, 
-      jump_height = 0#тормозит слегка, но работает
+   if  gravity == 0:
+      jump_height = 0
    if jump_trigger == True and jump_height < 3:
       jump_height += 1
       return -32
    return 16
 
 def corner_stop(x_speed, gravity):#Исправляет баг с углом
-   global level
    if gravity > 0:
      if x_speed > 0:
        if level.get_at(((x_cord+32)/32, (y_cord+32)/32)) <> (empty):
@@ -74,19 +71,24 @@ def corner_stop(x_speed, gravity):#Исправляет баг с углом
 def draw_player( x, y):#Функция отрисовки перса
    if x_speed == 0:
       if gravity <= 0:
-         player = p0
-      if gravity > 0:
-         player = p10
+         screen.blit(player, (x, y), (0,0,32,32))
+      elif gravity > 0:
+         screen.blit(player, (x, y), (0,33,32,32))
    global N
    if x_speed > 0:
-      if N==1: player = p1
-      elif N==2: player = p2
-      elif N==3: player = p3
+      if gravity == 0:
+         if N==1: screen.blit(player, (x, y), (33,0,32,32))
+         elif N==2: screen.blit(player, (x, y), (65,0,32,32))
+         elif N==3: screen.blit(player, (x, y), (93,0,32,32))
+      elif gravity <> 0:
+         screen.blit(player, (x, y), (33,0,32,32))
    if x_speed < 0:
-      if N==1: player = p4
-      elif N==2: player = p5
-      elif N==3: player = p6
-   screen.blit(player, (x, y))
+      if gravity == 0:
+         if N==1: screen.blit(player, (x, y), (33,33,32,32))
+         elif N==2: screen.blit(player, (x, y), (65,33,32,32))
+         elif N==3: screen.blit(player, (x, y), (93,33,32,32))
+      elif gravity <> 0:
+         screen.blit(player, (x, y), (33,33,32,32))
    N += 1
    if N >3: N=1
 
@@ -100,90 +102,96 @@ def camera_move(x,y,speed, gravity):
 
 def Spike():
    global HP
-   if level.get_at(((x_cord+16)/32, (y_cord+32)/32)) == (255,0,0) or level.get_at(((x_cord-16)/32, (y_cord+32)/32)) == (255,0,0):
-      HP -= 1
+   if level.get_at(((x_cord+16)/32, (y_cord+32)/32)) == (255,0,0):
+      HP -= 5
 
-
-#def Portal():
-#   if level.get_at(((x_cord+16)/32, (y_cord-32)/32)) == (200,170,8):
-#      print "Fuck Yeah!"
+def Lava():
+   global HP
+   if level.get_at(((x_cord+16)/32, (y_cord+32)/32)) == (255,128,0):
+      HP -= 20
+   if level.get_at(((x_cord+32)/32, (y_cord)/32)) == (255,128,0):
+      HP -= 20
+   if level.get_at(((x_cord-16)/32, (y_cord)/32)) == (255,128,0):
+      HP -= 20
 
 def draw_stats():
      HPstat = Stats_font.render("%d" % HP,0, (255,255,255))
      Keystat = Stats_font.render("%d" % Keys,0, (255,255,255))
-     screen.blit(Heart_image,(5, 448))
-     screen.blit(HPstat,(25, 448))
-     screen.blit(Key_image,(5, 424))
-     screen.blit(Keystat,(25, 424))
+     screen.blit(Heart_image,(10, 448))
+     screen.blit(HPstat,(30, 448))
+     screen.blit(Key_image,(10, 424))
+     screen.blit(Keystat,(30, 424))
 
 def Dead():
    if HP <= 0:
-     dead = Message_font.render("You die!",0, (255,255,255))
+     dead = Message_font.render("You died!",0, (255,255,255))
+     message = Stats_font.render("press x to restart" ,0, (255,255,255))
      screen.fill((0,0,0))
-     screen.blit(dead,(230, 200))
+     screen.blit(dead,(210, 200))
+     screen.blit(message,(205, 460))
+     if event.type == pygame.KEYDOWN:
+         if event.key == pygame.K_x: 
+            Load_level(lvl)
 
 def  Pick_key():
-   global Keys, key_list
+   global Keys, key_list,camera
    for i in key_list:
      camera.blit(GameKey,i)
      if (x_cord, y_cord) == i:
         Keys += 1
-        del key_list[key_list.index((x_cord, y_cord))] 
-#        camera.fill((0, 0, 0))
-
+        del key_list[key_list.index((x_cord, y_cord))]
 
 def next_level(key_list):
-   global lvl, camera, Keys, HP
-   if key_list == [] and level.get_at(((x_cord+16)/32, (y_cord-32)/32)) == (200,170,8):
-      print "Fuck Yeah!"
-      lvl = 3
-      camera = load_image("3background.png" )
-      HP=100
-      Keys = 0
-      key_list =[(320,706),(352,706)]
+   global lvl
+   if key_list == [] and level.get_at(((x_cord+16)/32, (y_cord-16)/32)) == (200,170,8):
+      lvl += 1
+      Load_level(lvl)
+
+def Load_level(lvl):
+   global level, camera, Keys, HP,key_list, x_cord, y_cord 
+   level = load_image("lvl%d.png" % lvl)
+   camera = load_image("background%d.png" % lvl)
+   HP=100
+   Keys = 0
+   key_list = list_of_lock[lvl]
+   x_cord, y_cord = list_of_cords[lvl]
+
+
+
 #____________________________________
 screen = pygame.display.set_mode([640, 480])
 pygame.display.set_caption('Crysis 4')
 
-camera = load_image("1background.png" )
-
-key_list =[(320,706),(352,706)]
+lvl=1
+list_of_lock =[[],[(1248,1090),(384,1058),(1728,802),(1984,642)],[(1216,416),(960,1120),(1920,1056)]]
+key_list = list_of_lock[lvl]
 GameKey = load_image("GameKey.png" )
 HP=100
 Keys = 0
-lvl=2
 Stats_font = pygame.font.Font("JFRocSol.ttf", 18)
 Message_font = pygame.font.Font("JFRocSol.ttf", 36)
 
 clock = pygame.time.Clock()
+camera = load_image("background%d.png" % lvl)
 level = load_image("lvl%d.png" % lvl)
 
 Heart_image = load_image("Heart.png")
 Key_image = load_image("key_stats.png")
 
 empty = (0, 0, 0)
-blue = (0,0,255)
-green = (0,255,0)
 
 #Скорость в пикселях за кадр
 x_speed = 0
 gravity = 16
 #Начальные корды
-x_cord = 704
-y_cord = 258
+x_cord, y_cord = 704, 258
+list_of_cords = [(0,0),(704, 258),(320, 256)]
 
 jump_trigger = True
 jump_height = 0
 
 N=1
-p10 = load_image("player10.png" )
-p0 = load_image("player0.png" )
-p1 = load_image("Rplayer1.png" )
-p2 = load_image("Rplayer2.png" )
-p3 = load_image("Rplayer3.png" )
-p4 = load_image("Lplayer1.png" )
-p5 = load_image("Lplayer2.png" )
-p6 = load_image("Lplayer3.png" )
+player = load_image("player.png" )
 
 done=False
 while done == False:
@@ -199,18 +207,16 @@ while done == False:
          if event.key == pygame.K_LEFT: x_speed = 0
          if event.key == pygame.K_RIGHT: x_speed = 0
 
-
     gravity =jump( gravity)
     x_speed = platform_func(x_speed)
     gravity = gravity_func(gravity)
     x_speed=corner_stop(x_speed, gravity)
     cam = camera_move( x_cord, y_cord, x_speed, gravity)
     Spike()
+    Lava()
     next_level(key_list)
-#    Dead()
-#    Portal()
-    print HP,key_list,lvl
 
+    print key_list,x_cord, y_cord
     x_cord += x_speed
     y_cord += gravity
 
